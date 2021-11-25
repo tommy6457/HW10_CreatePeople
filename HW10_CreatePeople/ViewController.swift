8b9dba21e2e5e67843cf43035c9a8134f3006c95
tommy <tommy5566123@gmail.com>
2021-11-25 23:53:58 +0800

    Initial Commit

---
+++ HW10_CreatePeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10_CreatePeople
+//
+//  Created by 蔡尚諺 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
